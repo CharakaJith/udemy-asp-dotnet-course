c71f432f89a7526963a6a8b3acc9bc9807d9f6bc
Charaka Gunasinghe <charaka.info@gmail.com>
2025-08-28 20:57:50 +0530

    section 01: getting started completed

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Random()
+        {
+            var movie = new Movies()
+            {
+                MovieId = 1,
+                Title = "Shrek"
+            };
+
+            return View(movie);
+        }
+    }
+}
