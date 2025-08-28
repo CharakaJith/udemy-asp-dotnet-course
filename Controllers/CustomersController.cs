e468050c8b7f989eca79c8504732b24839d87d97
Charaka Gunasinghe <charaka.info@gmail.com>
2025-08-29 00:39:13 +0530

    section 02: exercise completed

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customers> customers = new List<Customers>
+        {
+            new Customers
+            {
+                CustomerId = 1,
+                Name = "John Doe"
+            },
+            new Customers
+            {
+                CustomerId = 2,
+                Name = "Jane Doe"
+            },
+            new Customers
+            {
+                CustomerId = 3,
+                Name = "Bob Smith"
+            },
+        };
+
+        public IActionResult Index()
+        {
+            return View(this.customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Customers customer = this.customers.FirstOrDefault(c => c.CustomerId == id);
+
+            return View(customer);
+        }
+    }
+}
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,72 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Vidly.Models;
-using Vidly.Models.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index(int? pageIndex, string sortBy)
+        private readonly List<Movies> movies = new List<Movies>
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if (String.IsNullOrEmpty(sortBy))
-            {
-                sortBy = "name";
-            }
-
-            return Content(String.Format($"pageInde={pageIndex}&sortBy={sortBy}"));
-
-            //return View();
-        }
-
-        public IActionResult Random()
-        {
-            var movie = new Movies()
+            new Movies
             {
                 MovieId = 1,
                 Title = "Shrek"
-            };
-
-            var customers = new List<Customers>
+            },
+            new Movies
             {
-                new Customers
-                {
-                    CustomerId = 1,
-                    Name = "Customer A"
-                },
-                new Customers
-                {
-                    CustomerId = 2,
-                    Name = "Customer B"
-                },
-                new Customers
-                {
-                    CustomerId = 3,
-                    Name = "Customer C"
-                }
-            };
-
-            var viewModel = new RandomMovieViewModel
+                MovieId = 2,
+                Title = "Happy death day"
+            },
+            new Movies
             {
-                Movie = movie,
-                Customers = customers
-            };
+                MovieId = 3,
+                Title = "Wall-E"
+            },
+        };
 
-            return View(viewModel);
-        }
-
-        public IActionResult Edit(int id)
+        public IActionResult Index()
         {
-            return View();
-        }
-
-        [Route("movies/released/{year:int}/{month:int}")]
-        public IActionResult ByReleaseDate(int? year, int? month)
-        {
-            return Content($"{year} / {month}");
+            return View(this.movies);
         }
     }
 }
