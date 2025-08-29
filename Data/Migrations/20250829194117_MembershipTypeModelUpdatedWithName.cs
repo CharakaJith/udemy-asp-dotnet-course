816ecc13a617411b105ad4a82615ce2810f0bbb3
Charaka Gunasinghe <charaka.info@gmail.com>
2025-08-30 01:22:41 +0530

    section 03: exercise 1 completed

---
+++ Data/Migrations/20250829194117_MembershipTypeModelUpdatedWithName.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MembershipTypeModelUpdatedWithName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipType",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsSubscribedToNewsLetter",
+                table: "Customer",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipType");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsSubscribedToNewsLetter",
+                table: "Customer",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
---
+++ Data/Migrations/20250829194358_SeedMembershipTypeName.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMembershipTypeName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MembershipType",
+                keyColumn: "MembershipTypeId",
+                keyValue: 1,
+                columns: new[] { "Name" },
+                values: new object[] { "Free" }
+            );
+
+            migrationBuilder.UpdateData(
+                table: "MembershipType",
+                keyColumn: "MembershipTypeId",
+                keyValue: 2,
+                columns: new[] { "Name" },
+                values: new object[] { "Monthly" }
+            );
+
+            migrationBuilder.UpdateData(
+                table: "MembershipType",
+                keyColumn: "MembershipTypeId",
+                keyValue: 3,
+                columns: new[] { "Name" },
+                values: new object[] { "Quarterly" }
+            );
+
+            migrationBuilder.UpdateData(
+                table: "MembershipType",
+                keyColumn: "MembershipTypeId",
+                keyValue: 4,
+                columns: new[] { "Name" },
+                values: new object[] { "Yearly" }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
